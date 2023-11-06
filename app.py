fd4ba9975708cc1dedba73caeeb34748eebbbbb3
he-man86 <108829286+he-man86@users.noreply.github.com>
2023-11-06 22:01:35 +0100

    wrking

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import os
+import pickle
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import  HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-
-api_key = os.environ.get("OPENAI_API_KEY")
-print(api_key)
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -35,15 +33,26 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+    # # embeddings
+    store_name = 'vectorStore'
+    st.write(f'{store_name}')
+
+    if os.path.exists(f"{store_name}.pkl"):
+        with open(f"{store_name}.pkl", "rb") as f:
+            vectorStore = pickle.load(f)
+            st.write('Embeddings Loaded from the Disk')
+    else:
+        embeddings = HuggingFaceEmbeddings()
+        vectorStore = FAISS.from_texts(text_chunks, embedding=embeddings)
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(vectorStore, f)
+
+    return vectorStore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
@@ -52,6 +61,45 @@ def get_conversation_chain(vectorstore):
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
+    return conversation_chain
+
+def get_conversation_chain1(vectorstore):
+
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    # memory = ConversationBufferMemory(
+    #     memory_key='chat_history', return_messages=True)
+    # conversation_chain = ConversationalRetrievalChain.from_llm(
+    #     llm=llm,
+    #     retriever=vectorstore.as_retriever(),
+    #     memory=memory
+    # )
+    # return conversation_chain
+
+    model_name = "t5-small"
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    model.to('cpu')  # You can change 'cuda' to 'cpu' if you want to use CPU
+
+    llm = {
+            "model_name": "t5-small",  # Replace with your desired model name or path
+            "model_kwargs": {
+                "temperature": 0.5,
+                "max_length": 512
+            }
+        }
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history',
+        return_messages=True
+        )
+
+    conversation_chain = LLMChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+
     return conversation_chain
 
 
@@ -92,16 +140,12 @@ def main():
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
-
                 # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == '__main__':
